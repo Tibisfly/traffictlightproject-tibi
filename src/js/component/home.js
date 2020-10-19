5b0ad3f84aebb13ab72a26ed9ab68e052dcbacb7
Tibisfly <vielmatorotibisay@gmail.com>
2020-10-19 20:29:51 +0000

    the traffic light

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -2,23 +2,55 @@ import React from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
+import { useState, useEffect } from "react";
+import { checkPropTypes } from "prop-types";
 
 //create your first component
 export function Home() {
+	const [color, setColor] = useState("red");
+
 	return (
 		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
+			<h1>Traffic Light with React</h1>
+			<div className="theStick" />
+			<div className="container">
+				<span
+					className={
+						"traffictlightred " + (color == "red" ? "selected" : "")
+					}
+					onClick={() => setColor("red")}
+				/>
+				<br />
+				<span
+					className={
+						"traffictlightyellow " +
+						(color == "yellow" ? "selected" : "")
+					}
+					onClick={() => setColor("yellow")}
+				/>
+				<br />
+				<span
+					className={
+						"traffictlightgreen " +
+						(color == "green" ? "selected" : "")
+					}
+					onClick={() => setColor("green")}
+				/>
+			</div>
 			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
+				Made by <a href="http://www.4geeksacademy.com">Tibi</a>, with
 				love!
 			</p>
 		</div>
 	);
 }
+// (color == "red" ? "selected" : "") : es una forma "abreviada" de hacer un if, que sería así:
+//if(color == red){
+//   "selected"
+//}else{
+//    ""
+//};
+
+// className = {
+//     "traffictlightred " + (color == "red" ? "selected" : "")
+// } esto es modificar una clase (agrega o quita la clase selected)
